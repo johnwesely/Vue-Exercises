8d5715b35f7d443f905681293f738c745b0c7729
John Wesely <johnpwesely@gmail.com>
2022-03-09 11:05:31 -0700

    added comments to forms project

--- Forms/public/js/app.js
+++ Forms/public/js/app.js
@@ -2,16 +2,20 @@ class Errors {
     constructor() {
         this.errors = {};
     }
+
+    // gets the error for given field
     get(field) {
         if (this.errors[field]) {
             return this.errors[field][0];
         }
     }
 
+    // records errors to this
     record(errors) {
         this.errors = errors['errors'];
     }
 
+    // clears single fields errors if given or all errors
     clear(field) {
         if (field) {
             delete this.errors[field];
@@ -20,10 +24,12 @@ class Errors {
         }
     }
 
+    // returns true if given field has an error
     has(field) {
         return this.errors.hasOwnProperty(field);
     }
 
+    // returns true if any errors exist
     any() {
         return Object.keys(this.errors).length > 0;
     }
@@ -31,7 +37,7 @@ class Errors {
 
 class Form {
     constructor(data) {
-        this.originalData = data;
+        this.originalData = data;     // save original data for ability to dynamically access field
 
         for (let field in data) {
             this[field] = data[field];
@@ -40,6 +46,7 @@ class Form {
         this.errors = new Errors()
     }
 
+    // reset form
     reset() {
         for (let field in this.originalData) {
             this[field] = '';
@@ -48,6 +55,7 @@ class Form {
         this.errors.clear();
     }
 
+    // fetch relevant object for form
     data() {
         let data = Object.assign({}, this);
 
@@ -57,6 +65,7 @@ class Form {
         return data;
     }
 
+    // submmit data with requestType to url returns promise for handling by Vue instance
     submit(requestType, url) {
         return new Promise((resolve, reject) => {
             axios[requestType](url, this.data())
@@ -93,7 +102,7 @@ new Vue({
 
     methods: {
         onSubmit() {
-            this.form.submit('post', '/store')
+            this.form.submit('post', '/store')      //form.submit returns promise for handling
                 .then(data => console.log(data))
                 .catch(error=> console.log(error));
         }
